bc352d9d2b85c79633528d100281b823abaa54f8
Gustavovmdx <Gustavovmdx@gmail.com>
2023-10-21 01:53:26 -0500

    Segundo Commit

---
+++ ProyectoPI/Controllers/VistasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoPI.Controllers
+{
+    public class VistasController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Preguntas()
+        {
+            return View();
+        }
+
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
+
+    }
+
+}
