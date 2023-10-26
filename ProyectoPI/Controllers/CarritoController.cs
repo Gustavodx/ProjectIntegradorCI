c244e1eab0745c0fb5e80debed0e4855f6964950
Andree <andreechiquis11@gmail.com>
2023-10-25 21:21:49 -0500

    commit carrito simple

---
+++ ProyectoPI/Controllers/CarritoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoPI.Models;
+
+namespace ProyectoPI.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private readonly DBPRUEBAContext _context;
+
+        public CarritoController(DBPRUEBAContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var carrito = HttpContext.Session.Get<Carrito>("Carrito");
+            return View(carrito);
+        }
+
+    }
+}
